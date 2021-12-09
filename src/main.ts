2394a7ed4d94ef57d7cea9463f93a1cb9fa3ff95
koryagin_sergey <korjagin@technokratos.com>
2021-12-10 00:18:24 +0300

    database ssl required false

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { json, urlencoded } from 'express';
+import { defaults } from 'pg';
 import { AppModule } from './app.module';
+
+if (process.env.DATABASE_URL) {
+  defaults.ssl = { rejectUnauthorized: false }
+}
 
 async function bootstrap() {
   const PORT = process.env.PORT || 5001;
