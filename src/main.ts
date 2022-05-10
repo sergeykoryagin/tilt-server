4b2060fe3f196fc702b7ab401c74b0c2e4ac2a76
sergeykoryagin <48332027+sergeykoryagin@users.noreply.github.com>
2022-05-11 00:24:04 +0300

    test cd

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { json, urlencoded } from 'express';
 import { AppModule } from './app.module';
 
+
 async function bootstrap() {
   const PORT = process.env.PORT || 5001;
   const app = await NestFactory.create(AppModule);
