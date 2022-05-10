b73d401027518b2bba44d9c939a98ac93d6a1f14
koryagin_sergey <seregakid1990@gmail.com>
2022-05-11 02:02:58 +0300

    add endpoints to statistics controller

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,11 @@ import { NestFactory } from '@nestjs/core';
 import { json, urlencoded } from 'express';
 import { AppModule } from './app.module';
 
-
 async function bootstrap() {
-  const PORT = process.env.PORT || 5001;
-  const app = await NestFactory.create(AppModule);
-  app.use(json({ limit: '20mb' }));
-  app.use(urlencoded({ extended: true, limit: '20mb' }));
-  await app.listen(PORT);
+    const PORT = process.env.PORT || 5001;
+    const app = await NestFactory.create(AppModule);
+    app.use(json({ limit: '20mb' }));
+    app.use(urlencoded({ extended: true, limit: '20mb' }));
+    await app.listen(PORT);
 }
 bootstrap();
--- src/statistics/statistics.controller.ts
+++ src/statistics/statistics.controller.ts
@@ -1,9 +1,41 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Request, UseGuards } from '@nestjs/common';
+import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
 
 @Controller('statistics')
 export class StatisticsController {
-    @Get('kek')
-    async getStatistics() {
-        return 'statitics';
+    @Get('today')
+    @UseGuards(JwtAuthGuard)
+    async getTodayStatistics(@Request() request) {
+        return `today statistics for ${request.userId}`;
+    }
+
+    @Get('month')
+    @UseGuards(JwtAuthGuard)
+    async getMonthStatistics(@Request() request) {
+        return `month statistics for ${request.userId}`;
+    }
+
+    @Get('weekday')
+    @UseGuards(JwtAuthGuard)
+    async getWeekdayWithMostSmilingStatistics(@Request() request) {
+        return `the weekday with the most smiling messages for ${request.userId}`;
+    }
+
+    @Get('smile-frequency')
+    @UseGuards(JwtAuthGuard)
+    async getSmileFrequency(@Request() request) {
+        return `the smile frequency for ${request.userId}`;
+    }
+
+    @Get('user-i-sent')
+    @UseGuards(JwtAuthGuard)
+    async getUserISentTheMostMessages(@Request() request) {
+        return `the user i sent the most messages for ${request.userId}`;
+    }
+
+    @Get('user-who-sent')
+    @UseGuards(JwtAuthGuard)
+    async getUserWhoSentTheMostMessages(@Request() request) {
+        return `the user who sent to me the most messages for ${request.userId}`;
     }
 }
